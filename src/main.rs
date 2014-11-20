923660170f1747d63968f57b2ef4d2da7bbef1b1
Alexandru <alexandru.grigoras86@gmail.com>
2014-11-20 17:06:31 +0100

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
+extern crate time;
+
 use std::io;
+use std::rand;
 
 fn main() {
-    let mut array = [3,9,2,6,1,8,4,2,7];
+    let mut size:uint;
+    loop {
+        print!("Enter the dimension of the array to sort: ");
+        let input = io::stdin().read_line().ok().expect("Failed to read line");
+        let input_num:Option<uint> = from_str(input.as_slice().trim());
+        size =  match input_num {
+            Some(size) => {
+                size
+            },
+            None => {
+                println!("Please enter a positive number!");
+                continue;
+            }
+        };
+        break;
+    }
+    let mut array = vec![];
+    while array.len() < size {
+        array.push(rand::random::<int>() % 1000i)
+    }
     let len:uint = array.len();
-    print!("array before sort: ");
-    print_array(array);
-    quicksort(array, 0, len-1);
-    print!("array after sort: ");
-    print_array(array);
+    //print!("array before sort: ");
+    //print_array(array.as_mut_slice());
+    let start_time:time::Timespec = time::get_time();
+    quicksort(array.as_mut_slice(), 0, len-1);
+    let end_time:time::Timespec = time::get_time();
+    //print!("array after sort: ");
+    //print_array(array.as_mut_slice());
+    println!("{}",end_time.sub(&start_time))
 }
 
 fn partition(array:&mut[int], left:uint, right:uint) -> uint {
@@ -19,9 +44,15 @@ fn partition(array:&mut[int], left:uint, right:uint) -> uint {
     while i <= j {
         while array[i] < pivot {
             i = i+1u;
+            if i == array.len()-1 {
+                break;
+            }
         }
         while array[j] > pivot {
             j = j-1u;
+            if j == 0 {
+                break;
+            }
         }
         if i <= j {
             temp = array[i];
